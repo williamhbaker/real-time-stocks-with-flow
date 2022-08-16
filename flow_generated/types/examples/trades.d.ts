63b78a426a28a5f08c06fd7b75a9f9e50b0eedd7
Will Baker <wbaker@gmail.com>
2022-08-16 14:55:58 -0300

    catalog with sqlite

---
+++ flow_generated/types/examples/trades.d.ts
@@ -0,0 +1,8 @@
+
+// Generated from collection schema examples/flow.yaml?ptr=/collections/examples~1trades/schema.
+// Referenced from examples/flow.yaml#/collections/examples~1trades.
+export type Document = {
+    ID: number;
+    Symbol: string;
+};
+
