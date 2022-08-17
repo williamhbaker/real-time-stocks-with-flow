96e370a16e27402671461b5c99a5bc4658874464
Will Baker <wbaker@gmail.com>
2022-08-17 17:02:26 -0300

    example postgres capture for testing

--- flow_generated/types/examples/trades.d.ts
+++ flow_generated/types/examples/trades.d.ts
@@ -2,7 +2,13 @@
 // Generated from collection schema examples/flow.yaml?ptr=/collections/examples~1trades/schema.
 // Referenced from examples/flow.yaml#/collections/examples~1trades.
 export type Document = {
+    Conditions?: unknown[];
+    Exchange?: string;
     ID: number;
+    Price?: number;
+    Size?: number;
     Symbol: string;
+    Tape?: string;
+    Timestamp?: string;
 };
 
