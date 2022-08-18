f6c405259173a0a82f2e410cf4d9af06c0602ee7
Will Baker <wbaker@gmail.com>
2022-08-18 16:17:50 -0300

    fix node modules

--- flow/flow_generated/flow/routes.ts
+++ flow/flow_generated/flow/routes.ts
@@ -6,19 +6,14 @@ export type Lambda = (source: Document, register?: Document, previous?: Document
 
 // Import derivation classes from their implementation modules.
 import { Derivation as examplesAllTimeMinMaxPrice } from '../../minMax.flow';
-import { Derivation as examplesAllTimeMinMaxPrice2 } from '../../minMax2.flow';
 
 // Build instances of each class, which will be bound to this module's router.
 const __examplesAllTimeMinMaxPrice: examplesAllTimeMinMaxPrice = new examplesAllTimeMinMaxPrice();
-const __examplesAllTimeMinMaxPrice2: examplesAllTimeMinMaxPrice2 = new examplesAllTimeMinMaxPrice2();
 
 // Now build the router that's used for transformation lambda dispatch.
 const routes: { [path: string]: Lambda | undefined } = {
     '/derive/examples/allTimeMinMaxPrice/minMax/Publish': __examplesAllTimeMinMaxPrice.minMaxPublish.bind(
         __examplesAllTimeMinMaxPrice,
-    ) as Lambda,
-    '/derive/examples/allTimeMinMaxPrice2/minMax/Publish': __examplesAllTimeMinMaxPrice2.minMaxPublish.bind(
-        __examplesAllTimeMinMaxPrice2,
     ) as Lambda,
 };
 
