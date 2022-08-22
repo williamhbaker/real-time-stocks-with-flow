70f76c80785acf0eabe5b716c2422413f6dbaa2b
Will Baker <wbaker@gmail.com>
2022-08-22 09:14:36 -0300

    ui style

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import './App.sass';
 
 import { gql, useSubscription } from '@apollo/client';
 
@@ -32,19 +32,46 @@ function DisplayTrades() {
 
     return 0
   }).map(({ symbol, signal, price, trailingaverage }) => (
-    <p key={symbol}>
-      {symbol}: | Recommendation: {signal} | Avg: {trailingaverage.toLocaleString(undefined, { minimumFractionDigits: 2 })} | Price: {price.toLocaleString(undefined, { minimumFractionDigits: 2 })}
-    </p>
+      <div class="box has-background-light" key={symbol}>
+        <p class="title has-text-centered is-size-4">
+          {symbol}
+        </p>
+
+        <div class="level">
+
+          <div class="level-item has-text-centered">
+            <div>
+              <p class="heading is-size-6">Recommendation</p>
+              <p class={signal == "buy" ? "title is-size-5 has-text-success": "title is-size-5 has-text-danger"}>{signal.toUpperCase()}</p>
+            </div>
+          </div>
+
+          <div class="level-item has-text-centered">
+            <div>
+              <p class="heading is-size-6">All-Time Average Price</p>
+              <p class="title is-size-5">{trailingaverage.toLocaleString(undefined, { minimumFractionDigits: 2 })}</p>
+            </div>
+          </div>
+
+          <div class="level-item has-text-centered">
+            <div>
+              <p class="heading is-size-6">Last Traded At</p>
+              <p class="title is-size-5">{price.toLocaleString(undefined, { minimumFractionDigits: 2 })}</p>
+            </div>
+          </div>
+
+        </div>
+
+    </div>
   ));
 }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <DisplayTrades />
-      </header>
-    </div>
+
+  <section className="section">
+    <DisplayTrades />
+  </section>
   );
 }
 
